6761992886b946b35fc39e04b47c41721de3c7af
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 06:57:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,11 @@
 def reverse_each_word(strings)
   
-
-
+  new_array = []
+  
+  new_string = strings.split(" ")
+  
+  new_string.each do |string|
+    puts new_array << string.reverse
+  end 
+  new_array.join(" ")
 end 
