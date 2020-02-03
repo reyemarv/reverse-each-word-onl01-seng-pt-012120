309570503df35bb28076a532da2ef5170f168d22
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 06:26:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(strings)
   
   new_array = []
   
-  new_string = strings.split(",")
+  new_string = strings.split(" ")
   
   new_string.each do |string|
     new_array += "#{string.reverse}"
