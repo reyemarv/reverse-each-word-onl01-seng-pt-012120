c1d1549da5624f1c6701a657ac06c0f8d5c534e7
Marvin Reyes <reyemarv@gmail.com>
2020-02-03 06:05:05 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
 def reverse_each_word(words)
   
-    newest_words = []
-    
-    new_words = words.split(",")
-    
-    new_words.each do |word|
-      newest_words << word.reverse 
-    end
-    new_words.join
 end 
